9282b6871021d22dfc878587df352b91fbed1626
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-07-02 21:23:53 -0400

    read file from /api/notes and test with Thunderclient

--- server.js
+++ server.js
@@ -13,10 +13,23 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error'});
+        } else {
+            const parsedData = JSON.parse(data);
+            res.json(parsedData);
+        }
+    });
+});
+
+
+
 app.get('*', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
