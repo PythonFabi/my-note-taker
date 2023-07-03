7511b265f647bf4e87e26c1acc84d59728966f7c
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-07-02 20:59:53 -0400

    add the file path for /notes and change the index to a wildcard route

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => 
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
